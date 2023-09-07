b8d1a2de6ddb13132bb2f3398b34964be545435e
literally-bug-creator <w1shstudymail@gmail.com>
2023-09-07 19:15:10 +0300

    End location creation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    [Range(1f, 10f)]
+    private float _speed;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
